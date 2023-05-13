9315b3690a002daa0b2909df6fdbcc4336fa567b
Aidan Taylor <a.taylor41@saints.mhcc.edu>
2023-05-13 13:33:51 -0700

    Module 12 Sample Project

---
+++ module_12/sample_project/fitness-bootstrap/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Aidan Taylor
+    File Name: script.js
+    Date: May 13, 2023
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function() {
+    $('.hero').height($(window).height());
+});
